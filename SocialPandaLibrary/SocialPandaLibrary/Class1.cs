a887e481066b885905dad278378d30c6f0150277
ivanov961 <ruski9606@gmail.com>
2016-01-21 19:50:53 +0200

    FirstCommit

---
+++ SocialPandaLibrary/SocialPandaLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialPandaLibrary
+{
+    public class Class1
+    {
+    }
+}
