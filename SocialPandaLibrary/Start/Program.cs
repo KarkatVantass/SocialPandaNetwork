e4af7aeebd6603467e049739d1069858d9684395
ivanov961 <ruski9606@gmail.com>
2016-01-23 12:51:47 +0200

    test

--- SocialPandaLibrary/Start/Program.cs
+++ SocialPandaLibrary/Start/Program.cs
@@ -10,8 +10,26 @@ namespace Start
     {
         static void Main(string[] args)
         {
-            Panda ruski = new Panda("Ruski", "ruski@abv.bg", Gender.male);
-            Console.WriteLine(ruski.ToString());
+            Panda ruski = new Panda("Ruski", "ivan@gmail.com", Gender.male);
+            Panda ruski1 = new Panda("One", "ivan@gmail.com", Gender.male);
+            Panda ruski2= new Panda("Two", "rusk2@abv.bg", Gender.male);
+            Panda ruski3 = new Panda("Three", "rusk3@abv.bg", Gender.male);
+            Panda ruski4 = new Panda("foure", "rusk4@abv.bg", Gender.male);
+            Panda ruski5 = new Panda("Five", "rusk5@abv.bg", Gender.male);
+            PandaSocialNetwork pncc = new PandaSocialNetwork();
+            pncc.AddPanda(ruski);
+            pncc.AddPanda(ruski1);
+            pncc.AddPanda(ruski2);
+            pncc.AddPanda(ruski3);
+            pncc.AddPanda(ruski4);
+            pncc.AddPanda(ruski5);
+    
+            pncc.MakeFriends(ruski, ruski2);
+            pncc.MakeFriends(ruski2, ruski3);
+            pncc.MakeFriends(ruski3, ruski4);
+            pncc.MakeFriends(ruski4, ruski5);
+            Console.WriteLine(pncc.ConnectionLevel(ruski1, ruski5));
+            Console.WriteLine(pncc.AreConnected(ruski1,ruski5));
         }
     }
 }
